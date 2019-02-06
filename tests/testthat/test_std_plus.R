ffda186d89850a9b272474898ef6a3722abc71a4
mru4913 <albertyu4913@gmail.com>
2019-02-05 22:45:34 -0800

    Update

---
+++ tests/testthat/test_std_plus.R
@@ -0,0 +1,11 @@
+context("Binding factors")
+
+test_that("fbind binds factor (or character)", {
+  x <- c('a', 'b')
+  x_fact <- factor(x)
+  y <- c('c', 'd')
+  z <- factor(c('a', 'b', 'c', 'd'))
+
+  expect_identical(std_plus(x, y), z)
+  expect_identical(std_plus(x_fact, y), z)
+})
