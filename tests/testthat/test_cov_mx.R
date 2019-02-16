b199e9975e2ea5de58a1d8cf4401bb38f7111763
mru4913 <albertyu4913@gmail.com>
2019-02-15 16:32:59 -0800

    Update documentation of the code

--- tests/testthat/test_cov_mx.R
+++ tests/testthat/test_cov_mx.R
@@ -7,11 +7,16 @@ test_that("Test valid data format",{
   single_row_x <- matrix(c(-2,-1,0,1,2), 1, 5)
   char_x <- as.character(x)
 
-  expect_true(is.na(cov_mx(single_row_x)[1,1])) # expect NA if we have a single row in the input matrix
-  expect_equal(dim(cov_mx(x))[1], ncols) # expect the covariance matrix has the same number of rows as the number of features of input matrix
-  expect_equal(dim(cov_mx(x))[2], ncols) #  expect the covariance matrix has the same number of cols as the number of features of input matrix
-  expect_true(is.matrix(cov_mx(x)))# expect a matrix output
-  expect_error(cov_mx(char_x)) #  expect ERROR if the input matrix is not numeric
+  # expect NA if we have a single row in the input matrix
+  expect_true(is.na(cov_mx(single_row_x)[1,1]))
+  # expect the covariance matrix has the same number of rows as the number of features of input matrix
+  expect_equal(dim(cov_mx(x))[1], ncols)
+  # expect the covariance matrix has the same number of cols as the number of features of input matrix
+  expect_equal(dim(cov_mx(x))[2], ncols)
+  # expect a matrix output
+  expect_true(is.matrix(cov_mx(x)))
+  # expect ERROR if the input matrix is not numeric
+  expect_error(cov_mx(char_x))
 })
 
 test_that("Test the correctness of the function cov_mc", {
@@ -21,9 +26,10 @@ test_that("Test the correctness of the function cov_mc", {
 
   good_cov_result <- cov_mx(good_x)
 
+  # expect TRUE for all covariance in the matrix
   for (i in 1:nrows){
     for(j in 1:ncols){
-      expect_equal(good_cov_result[i,j], cov(good_x[,i], good_x[,j])) # expect TRUE for all covariance in the matrix
+      expect_equal(good_cov_result[i,j], cov(good_x[,i], good_x[,j]))
     }
   }
 
