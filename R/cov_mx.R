5866af4c5b375ad605e1f3157b4fcfb1a1cc6dfe
keray3c1 <ckera.yucel@gmail.com>
2019-02-16 18:58:30 -0800

    function conditions are complete

--- R/cov_mx.R
+++ R/cov_mx.R
@@ -1,12 +1,49 @@
+#' Covariance matrix
+#'
 #' `cov_mx` function calculates the covariance matrix of the two
 #'  variables and automatically deals with the missing value
 #'
+#' @usage cov_mx(x)
+#'
 #' @param x a matrix that contains one or more than one random variable
 #'
-#' @return  matrix that contains the covariance between random variables in the input matrix
+#' @return z matrix that contains the covariance between random
+#' variables in the input matrix
+#'
 #' @export
 #'
-#' @examples
+#' @examples cov_mx(matrix(c(1, 2, NA, 4, 5, 6, 7, 8), 4))
 cov_mx <- function(x){
-  message("running cov_mx")
+
+  # check whether input vectors are valid or not
+  if(is.null(x)){
+    stop("Input cannot be empty.")
+  }
+
+  # the input data should be numeric
+  if(!(is.numeric(x) || is.logical(x))) stop("'x' must be numeric.")
+  stopifnot(is.atomic(x))
+
+   # return ERROR if the inputs are empty
+  if(length(x) == 0L){
+    stop("'x' must be non-empty.")
+  }
+
+  # return NA if The length of input equal to 1
+  else if (length(x) == 1L){
+    return (NA)
+  }
+
+  # return error if matrix is not 2D
+  if (dim(x)[2] == 1){
+    stop("'x' must be 2D matrix")
+  }
+  else if (dim(x)[2] > 2){
+    stop("'x' must be 2D matrix")
+  }
+
+
+
+
+
 }
